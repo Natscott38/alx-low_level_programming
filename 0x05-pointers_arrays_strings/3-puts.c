ce8435f1d0cf35d3b6de77ddccdf1a551334bc3f
natscott38 <natscott383@gmail.com>
2022-12-21 10:11:55 +0300

    files

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: the string in question
+ * Return: the length of the string
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0' str++)
+	{
+		_putchar(*str);
+	}
+	_putchar(\n);
+}
